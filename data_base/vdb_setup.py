720774b85fb2ac07fb4443fe22bbe5487815c4e2
Marcos Pifer <marcos.pifer.bignhardi@gmail.com>
2026-01-17 13:14:43 +0100

    move file

--- data_base/vdb_setup.py
+++ data_base/vdb_setup.py
@@ -1,16 +1,25 @@
-import chromadb
+from langchain.vectorstores import Chroma
+from langchain.embeddings import SentenceTransformerEmbeddings
 from config import DATABASE_NAME, TABLE_NAME, SENTENCE_TRANSFORMER_MODEL
 
 
 def setup_vector_db():
-    client = chromadb.PersistentClient(DATABASE_NAME)
+    # client = chromadb.PersistentClient(DATABASE_NAME)
 
-    embed_fn = chromadb.utils.embedding_functions \
-        .SentenceTransformerEmbeddingFunction(model_name=SENTENCE_TRANSFORMER_MODEL)
+    # embed_fn = chromadb.utils.embedding_functions \
+    #     .SentenceTransformerEmbeddingFunction(model_name=SENTENCE_TRANSFORMER_MODEL)
 
-    client.create_collection(
-        TABLE_NAME,
-        embedding_function=embed_fn
+    # client.create_collection(
+    #     TABLE_NAME,
+    #     embedding_function=embed_fn
+    # )
+
+    embedding = SentenceTransformerEmbeddings(model_name=SENTENCE_TRANSFORMER_MODEL)
+    persist_directory = DATABASE_NAME
+
+    vectordb = Chroma(
+        persist_directory=persist_directory,
+        embedding_function=embedding
     )
 
 if __name__ == "__main__":
